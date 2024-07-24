6d0250762150837f27b51e10cf951b9101857cd4
jeonwon <jeonwons@hanmail.net>
2024-07-24 16:42:35 +0900

    page에 Home컴포넌트 수정

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,8 @@
 export default function Home() {
-  return <main>
-    <h1>안녕하세요 메인입니다.</h1>
-  </main>;
+  return (
+    <main>
+      <h1>안녕하세요 메인입니다.</h1>
+      <h3>저는 서울 중랑구 망우로에 있습니다</h3>
+    </main>
+  );
 }
